408fd0082e419c9f7501316a3b5bb2ab7d209fa8
Zorayr Khalapyan <zkhalapyan@gmail.com>
2012-05-24 19:47:05 -0700

    Added Android PhoneGap Eclipse project source.

---
+++ android/gen/org/ohmage/mwoc/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.ohmage.mwoc;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/src/org/ohmage/mwoc/MWoCActivity.java
@@ -0,0 +1,14 @@
+package org.ohmage.mwoc;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MWoCActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+        //super.loadUrl("file:///android_asset/www/test-camera.html");
+    }
+}
