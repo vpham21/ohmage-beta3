c8d7d4c872a8c2bd12128319874b1c3ae674dcc7
zkhalapyan <zkhalapyan@gmail.com>
2012-08-14 01:19:56 -0700

    Modified HelpSection and SurveyResponseView intitializers to go back to the last page if page properties were not correctly set up - this should solve blank page issues when going back on Android devices (issue #95).

--- src/www/js/init/init.SurveyResponseView.js
+++ src/www/js/init/init.SurveyResponseView.js
@@ -1,5 +1,6 @@
 invokeOnReady(function(){
     var surveyKey = PageNavigation.getPageParameter('survey-key');
+    if(surveyKey === null){ PageNavigation.goBack(); }
     var surveyResponse = SurveyResponseModel.restoreSurveyResponse(surveyKey);
     var surveyResponseController = new SurveyResponseController(surveyResponse);
     document.getElementById("survey-response-view").appendChild(surveyResponseController.render());
