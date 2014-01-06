22f66e6ebb0cd84d5368ccdd85e728177aefe714
Zorayr Khalapyan <zkhalapyan@gmail.com>
2014-01-05 22:02:57 -0800

    Fix Help Menu Top Button Offset Issue.

--- src/projects/mobilize/ios/Mobilize/www/js/init/init.HelpMenu.js
+++ src/projects/mobilize/ios/Mobilize/www/js/init/init.HelpMenu.js
@@ -1,4 +1,6 @@
 
 Init.invokeOnReady(function() {
     $('#help-container').append((new HelpController()).renderHelpMenu());
+                   
+    mwf.decorator.TopButton("Dashboard" , null, PageNavigation.openDashboard, true);
 });
