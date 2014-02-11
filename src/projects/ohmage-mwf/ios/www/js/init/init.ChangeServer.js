bffeb0a88c78a1a8213ef8538eb9711a192405c2
Vinh Pham <vinh@Webmac2.local>
2014-02-10 17:04:49 -0800

    Fixed the Change-Server page to redirect to the correct page.

--- src/projects/ohmage-mwf/ios/www/js/init/init.ChangeServer.js
+++ src/projects/ohmage-mwf/ios/www/js/init/init.ChangeServer.js
@@ -1,4 +1,3 @@
 Init.invokeOnReady(function() {
     $('#view').append((new ChangeServerController()).renderChangeServerView());
-    mwf.decorator.TopButton("Login", null, PageNavigation.openAuthenticationPage(), true);
 });
--- src/projects/ohmage-mwf/ios/www/js/init/init.Profile.js
+++ src/projects/ohmage-mwf/ios/www/js/init/init.Profile.js
@@ -1,3 +1,4 @@
 Init.invokeOnReady(function() {
     $('#view').append(ProfileController().renderProfileView());
+    mwf.decorator.TopButton("Dashboard", null, PageNavigation.openDashboard, true);
 });
